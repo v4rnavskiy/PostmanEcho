c618770d9ce3455a8c53b9980dea95f15e02d016
Evgeniy <varnavskiyev@gmail.com>
2023-01-11 12:13:54 +0300

    added test

---
+++ src/test/java/ru/netology/postmanecho/MobileBankTestAPIPost.java
@@ -0,0 +1,22 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankTestAPIPost {
+    @Test
+    void shouldReturnSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("when I start to understand something")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("when I start to understand something"))
+        ;
+
+    }
+}
